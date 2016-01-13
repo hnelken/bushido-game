5d6c665a9ab52563970309093280467250eb98ff
Harry Nelken <harry.nelken@gmail.com>
2016-01-12 20:06:58 -0500

    beginning of local multiplayer mechanic
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

---
+++ Assets/NaiveGameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaiveGameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Player 1 input
+		if (Input.GetKeyDown (KeyCode.S)) {
+
+		}
+
+		// Player 2 input
+		if (Input.GetKeyDown (KeyCode.K)) {
+
+		}
+
+	}
+}
