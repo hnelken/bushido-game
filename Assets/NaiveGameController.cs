a3da6e97eb92e477a30d8084c199ffcce35c7cb1
Harry Nelken <harry.nelken@gmail.com>
2016-01-12 20:37:27 -0500

    removed unnecessary git adds and setup player input
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- Assets/NaiveGameController.cs
+++ Assets/NaiveGameController.cs
@@ -3,23 +3,28 @@ using System.Collections;
 
 public class NaiveGameController : MonoBehaviour {
 
+	public GameObject leftSamurai, rightSamurai;
+	
+	private enum LastWinner { LEFT, RIGHT }
+
+	private bool waitingForInput;
+	private bool flagPopped;
+	private LastWinner lastWinner;
+
 	// Use this for initialization
 	void Start () {
-	
+		waitingForInput = false;
+		flagPopped = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// Player 1 input
-		if (Input.GetKeyDown (KeyCode.S)) {
+	}
+
+	public void SamuraiTrigger(bool leftSamurai) {
+		if (waitingForInput) {
 
 		}
-
-		// Player 2 input
-		if (Input.GetKeyDown (KeyCode.K)) {
-
-		}
-
 	}
 }
---
+++ Assets/SamuraiControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamuraiControl : MonoBehaviour {
+
+	public bool leftSamurai;
+
+	private NaiveGameController controller;
+
+	public void SetController(NaiveGameController controller) {
+		this.controller = controller;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Player 1 input
+		if (leftSamurai) {
+			if (Input.GetKeyDown(KeyCode.S)) {
+				SamuraiReact();
+			}
+		} 
+		else {	// Player 2 input
+			if (Input.GetKeyDown(KeyCode.K)) {
+				SamuraiReact();
+			}
+		}
+	}
+
+	// Signal a player input to the game controller
+	private void SamuraiReact() {
+		controller.SamuraiTrigger(leftSamurai);
+	}
+
+}
