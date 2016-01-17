c9f9d5040fb911269b54d49852512ab61cda985c
Harry Nelken <harry.nelken@gmail.com>
2016-01-16 21:40:22 -0500

    got independent input recognized on all clients, working on game manager
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

---
+++ Assets/GameManager.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	private enum LastWinner { LEFT, RIGHT }
+	
+	private LastWinner lastWinner;
+
+	private bool waitingForRestart;
+	private bool waitingForInput;
+	private bool flagPopped;
+	private bool restarting;
+
+	#region State Methods
+
+	// Use this for initialization
+	void Start () {
+		waitingForRestart = false;
+		waitingForInput = false;
+		flagPopped = false;
+		restarting = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// End of overrides methods
+	#endregion
+
+
+	#region Public API Methods
+
+	// Returns an instance of the game manager
+	public static GameManager Get()
+	{
+		return GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+
+	// Signal game manager to refresh for new 
+	public void RefreshForNewRound() 
+	{
+
+	}
+
+	// Signal game manager that new round timer can begin
+	public void BeginNewRound() {
+		restarting = false;
+		waitingForInput = true;
+		StartCoroutine(WaitAndPopFlag());
+	}
+
+	public void TriggerReaction()
+	{
+		if (waitingForInput) 
+		{
+			// Accept no more input
+			waitingForInput = false;
+			
+			if (flagPopped)
+			{	// Player reacted after flag popped (win)
+				TriggerWin();	
+			}
+			else 
+			{	// Player reacted before flag popped (strike)
+				TriggerStrike();
+			}
+		}
+	}
+
+	// End of public API
+	#endregion
+
+
+	#region Yield Methods
+
+	// Waits for the initial animations to complete and begins a new round
+	public IEnumerator WaitAndStartRound() {
+		
+		yield return new WaitForSeconds(5f);
+		
+		BeginNewRound();
+	}
+
+	// Waits a random time and pops the center flag
+	public IEnumerator WaitAndPopFlag()
+	{
+		float randomWait = Random.Range(2, 8);
+		yield return new WaitForSeconds(randomWait);
+
+		flagPopped = true;
+	}
+
+	// Waits a short time after samurais cross to show winner
+	public IEnumerator WaitAndShowWinner() 
+	{
+		yield return new WaitForSeconds(2);
+		
+		switch (lastWinner)
+		{
+		case LastWinner.LEFT:
+			break;
+		case LastWinner.RIGHT:
+			break;
+		}
+		
+		waitingForRestart = true;
+	}
+
+	// End of yields
+	#endregion
+
+
+	#region Private Methods
+
+	// Signal that a player reacted after the flag was popped
+	private void TriggerWin() 
+	{
+		flagPopped = false;
+		
+		StartCoroutine(WaitAndShowWinner());
+	}
+
+	// Signal that a player reacted preemptively
+	private void TriggerStrike()
+	{
+		// BYPASSING STRIKE CURRENTLY
+		waitingForInput = true;
+
+		//flagPopped = false;
+		//waitingForRestart = true;
+	}
+
+	// End of private methods
+	#endregion
+}
--- Assets/scene_NetDuel/source/TouchControl.cs
+++ Assets/scene_NetDuel/source/TouchControl.cs
@@ -1,19 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class TouchControl : MonoBehaviour {
+public class TouchControl : NetworkBehaviour {
 
 	private bool touching;
+	public override void OnStartLocalPlayer()
+	{
+		// Initialize
+		touching = false;
+	}
 
 	// Use this for initialization
 	void Start () {
-		touching = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!touching && Input.touchCount > 0) {
+
+		if (!isLocalPlayer) 
+		{
+			return;
+		}
+
+		CheckInput();
+	}
+
+	private void CheckInput() 
+	{
+		// Check for input via touch or keyboard
+		if (!touching && (Input.touchCount > 0 || 
+		                  Input.GetKeyDown(KeyCode.Space)))
+		{
 			touching = true;
+			CmdTriggerReaction();
+			
+		}	// Also check for end of input
+		else if (touching && (Input.touchCount == 0 ||
+		                      Input.GetKeyUp(KeyCode.Space)))
+		{
+			touching = false;
 		}
 	}
+
+	[Command]
+	void CmdTriggerReaction()
+	{
+		RpcReceiveReaction();
+	}
+
+	[ClientRpc]
+	void RpcReceiveReaction()
+	{
+		GameManager.Get().TriggerReaction();
+	}
+
+
 }
