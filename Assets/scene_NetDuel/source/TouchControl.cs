de47dda9c12c3b84c38bf5295dd3cb4bdd4781b2
Harry Nelken <harry.nelken@gmail.com>
2016-01-16 19:35:39 -0500

    organized file structure and began a networked duel scene
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

---
+++ Assets/scene_NetDuel/source/TouchControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControl : MonoBehaviour {
+
+	private bool touching;
+
+	// Use this for initialization
+	void Start () {
+		touching = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!touching && Input.touchCount > 0) {
+			touching = true;
+		}
+	}
+}
