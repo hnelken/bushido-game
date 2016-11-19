ae163be9e4171a0a9c2f193d8d238e95154fc697
Harry Nelken <harry.nelken@gmail.com>
2016-11-19 02:59:33 -0500

    moved network manager to menu scene, starting custom match maker
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- Assets/source/managers/BushidoNetManager.cs
+++ Assets/source/managers/BushidoNetManager.cs
@@ -1,7 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 using System.Collections;
 
 public class BushidoNetManager : NetworkManager {
-	
+
+	public void StartupHost() {
+		SetPort();
+		NetworkManager.singleton.StartHost();
+	}
+
+	public void JoinGame() {
+		SetIPAddress();
+		SetPort();
+		NetworkManager.singleton.StartClient();
+	}
+
+	private void SetIPAddress() {
+		string ipAddress = GameObject.Find("ipAddressField").transform.FindChild("Text").GetComponent<Text>().text;
+		NetworkManager.singleton.networkAddress = ipAddress;
+	}
+
+	private void SetPort() {
+		NetworkManager.singleton.networkPort = 7777;
+	}
+
+
 }
