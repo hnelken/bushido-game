66f20b046ea6c6aa6c7d89f5894e81c1352a4c0e
Harry Nelken <harry.nelken@gmail.com>
2017-08-04 18:13:56 -0400

    setup button events and menu selections for solo lobby
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- Assets/source/reworked/SoloLobbyManager.cs
+++ Assets/source/reworked/SoloLobbyManager.cs
@@ -19,6 +19,9 @@ public class SoloLobbyManager : MonoBehaviour {
 	#region Private Variables
 
 	private int colorIndex;
+	private Color[] colorOptions = {
+		Color.blue, Color.yellow, Color.white, Color.red, Color.magenta, Color.green, Color.cyan
+	};
 	private int speedIndex;
 	private string[] speedOptions = {
 		"SLOW", "NORMAL", "FAST"
@@ -86,6 +89,76 @@ public class SoloLobbyManager : MonoBehaviour {
 	protected void SetInteractiveUIVisible(bool visible) {
 		//LeftArrow.gameObject.SetActive(visible);
 		//RightArrow.gameObject.SetActive(visible);
+	}
+
+	// Change the current win limit selection
+	protected void ChangeSpeedIndex(bool minus) {
+		speedIndex = ChangeIndex(speedIndex, speedOptions.Length, minus);
+		UpdateSpeedText();
+	}
+
+	protected void ChangeColorIndex(bool minus) {
+		colorIndex = ChangeIndex(colorIndex, colorOptions.Length, minus);
+		UpdatePlayerColor();
+	}
+
+	// Change the current win limit selection
+	protected int ChangeIndex(int index, int max, bool minus) {
+		// Changing match parameters resets ready status
+		countdown.ClearReadyStatus();
+
+		// Increment or decrement the index with wraparound
+		if (minus) {
+			if (index > 0) {
+				return index - 1;
+			}
+			else {
+				return max - 1;
+			}
+		}
+		else {
+			if (index < max - 1) {
+				return index + 1;
+			}
+			else {
+				return 0;
+			}
+		}
+	}
+
+	// Updates player color on the lobby
+	protected void UpdateSpeedText() {
+		GameSpeedText.text = speedOptions[speedIndex];
+	}
+	
+	// Updates difficulty text on the lobby
+	protected void UpdatePlayerColor() {
+		PlayerImage.color = colorOptions[colorIndex];
+	}
+
+	#endregion
+
+
+	#region Button Events
+
+	public void OnColorLeftPressed() {
+		Globals.Audio.PlayMenuSound();
+		ChangeColorIndex(true);
+	}
+
+	public void OnColorRightPressed() {
+		Globals.Audio.PlayMenuSound();
+		ChangeColorIndex(false);
+	}
+
+	public void OnSpeedLeftPressed() {
+		Globals.Audio.PlayMenuSound();
+		ChangeSpeedIndex(true);
+	}
+
+	public void OnSpeedRightPressed() {
+		Globals.Audio.PlayMenuSound();
+		ChangeSpeedIndex(false);
 	}
 
 	#endregion
